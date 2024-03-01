22497a8c9e911a355c2ca414488aa88dc5c15f91
Alex N <xander.nesta@gmail.com>
2024-03-01 14:52:36 -0500

    add favicon and vite.config.js settings to be able to host on glitch.me

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], //settings below in order to host site on glitch.me
+  build: {
+    outDir: "build"
+  },
+  server: {
+    host:"0.0.0.0",
+    port:3000,
+    strictPort: true,
+    hmr: {
+      clientPort: 443 // Run the websocket server on the SSL port
+    }
+  }
 })
